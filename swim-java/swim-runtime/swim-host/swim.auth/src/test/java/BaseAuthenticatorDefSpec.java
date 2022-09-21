35fa8d2c6d80f524f51508846e70b0cde9c240ab
Dobromir Marinov <mr.d.marinov@gmail.com>
2022-09-21 13:38:26 +0100

    * Fixed unit tests.

--- swim-java/swim-runtime/swim-host/swim.auth/src/test/java/BaseAuthenticatorDefSpec.java
+++ swim-java/swim-runtime/swim-host/swim.auth/src/test/java/BaseAuthenticatorDefSpec.java
@@ -54,7 +54,7 @@ public class BaseAuthenticatorDefSpec {
 
   @Test
   public void testBaseAuthOpenIdForm() {
-    final BaseAuthenticatorDef expectedDef = getBaseAuthenticatorGoogleDef();
+    final BaseAuthenticatorDef expectedDef = getBaseAuthenticatorOpenIdDef();
     final Form<BaseAuthenticatorDef> form = BaseAuthenticatorDef.form();
     final Item items = form.mold(expectedDef);
     final BaseAuthenticatorDef actualDef = form.cast(items);
@@ -64,7 +64,7 @@ public class BaseAuthenticatorDefSpec {
 
   @Test
   public void testBaseAuthOpenIdFormFromFile() throws IOException {
-    final BaseAuthenticatorDef expectedDef = getBaseAuthenticatorGoogleDef();
+    final BaseAuthenticatorDef expectedDef = getBaseAuthenticatorOpenIdDef();
     final Form<BaseAuthenticatorDef> form = BaseAuthenticatorDef.form();
     final BaseAuthenticatorDef actualDef = form.cast(TestUtils.readReconAuthSpec("base-auth-open-id.recon"));
 
@@ -84,27 +84,21 @@ public class BaseAuthenticatorDefSpec {
   }
 
   static BaseAuthenticatorDef getBaseAuthenticatorGoogleDef() {
-    FingerTrieSeq<PublicKeyDef> keys = FingerTrieSeq.empty();
-    keys = keys.appended(new RsaPublicKeyDef(BigInteger.valueOf(123), BigInteger.valueOf(456)));
-    keys = keys.appended(new RsaPublicKeyDef(BigInteger.valueOf(321), BigInteger.valueOf(654)));
-
     HashTrieMap<String, FingerTrieSeq<String>> claims = HashTrieMap.empty();
-    claims = claims.updated("client_id", FingerTrieSeq.of("some_client_id"));
-    claims = claims.updated("iss", FingerTrieSeq.of("https://example.com"));
-    claims = claims.updated("token_use", FingerTrieSeq.of("first_access", "second_access"));
-    return new BaseAuthenticatorDef("auth", "custom_access_token", "custom_exp", claims, keys, Uri.parse("https://test.com"), HttpSettings.standard());
+    claims = claims.updated("aud", FingerTrieSeq.of("custom_google_audience"));
+    claims = claims.updated("email", FingerTrieSeq.of("email_1@google.com", "email_2@google.com"));
+    return new BaseAuthenticatorDef("google", "idToken", "exp", claims, FingerTrieSeq.empty(), Uri.parse("https://www.googleapis.com/oauth2/v3/certs"), HttpSettings.standard());
   }
 
   static BaseAuthenticatorDef getBaseAuthenticatorOpenIdDef() {
     FingerTrieSeq<PublicKeyDef> keys = FingerTrieSeq.empty();
-    keys = keys.appended(new RsaPublicKeyDef(BigInteger.valueOf(123), BigInteger.valueOf(456)));
-    keys = keys.appended(new RsaPublicKeyDef(BigInteger.valueOf(321), BigInteger.valueOf(654)));
+    keys = keys.appended(new RsaPublicKeyDef(BigInteger.valueOf(987), BigInteger.valueOf(654)));
+    keys = keys.appended(new RsaPublicKeyDef(BigInteger.valueOf(432), BigInteger.valueOf(210)));
 
     HashTrieMap<String, FingerTrieSeq<String>> claims = HashTrieMap.empty();
-    claims = claims.updated("client_id", FingerTrieSeq.of("some_client_id"));
-    claims = claims.updated("iss", FingerTrieSeq.of("https://example.com"));
-    claims = claims.updated("token_use", FingerTrieSeq.of("first_access", "second_access"));
-    return new BaseAuthenticatorDef("auth", "custom_access_token", "custom_exp", claims, keys, Uri.parse("https://test.com"), HttpSettings.standard());
+    claims = claims.updated("aud", FingerTrieSeq.of("open_id_audience_1", "open_id_audience_2"));
+    claims = claims.updated("iss", FingerTrieSeq.of("open_id_issuer"));
+    return new BaseAuthenticatorDef("open_id", "idToken", "exp", claims, keys, Uri.parse("https://open-id.com"), HttpSettings.standard());
   }
 
 }
