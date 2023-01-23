cee67b3987c0f40896e2ad3bf2d937dd8af6cbdf
ajay-gov <ajay.gov@protonmail.com>
2023-01-23 14:21:55 -0800

    Update copyright year Add missing copyright in files

--- swim-java/swim-runtime/swim-core/swim.http/src/main/java/swim/http/MediaRangeWriter.java
+++ swim-java/swim-runtime/swim-core/swim.http/src/main/java/swim/http/MediaRangeWriter.java
@@ -1,4 +1,4 @@
-// Copyright 2015-2022 Swim.inc
+// Copyright 2015-2023 Swim.inc
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
